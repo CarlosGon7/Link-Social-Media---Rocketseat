3c8a2ff7eeeaacf1d47e93cce754d90597bb1894
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-01-12 13:01:53 -0300

    Adiciona ícones de redes sociais

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle("light");
+
+    // Pegar a img
+    const img = document.querySelector("#profile img");
+
+    // Verifica se o elemento html possui light mode
+    if (html.classList.contains("light")){
+        img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+        img.setAttribute("src", "./assets/avatar.png")
+    }
+}
+
+
